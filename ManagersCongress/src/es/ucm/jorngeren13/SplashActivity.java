402f26d83e2f38f2a57243dcb210ea0db5400607
Samuel <samuelmgalan@gmail.com>
2013-11-10 18:22:49 +0100

    Eliminated one warning

--- ManagersCongress/src/es/ucm/jorngeren13/SplashActivity.java
+++ ManagersCongress/src/es/ucm/jorngeren13/SplashActivity.java
@@ -2,7 +2,6 @@ package es.ucm.jorngeren13;
 
 import com.actionbarsherlock.view.Window;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
